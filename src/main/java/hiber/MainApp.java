3f992e7e7fefb21ac1f334b45ed7a45682ff2462
polchaev <polchaev@users.noreply.github.com>
2020-10-31 18:53:49 +0300

    Complete

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -44,7 +44,6 @@ public class MainApp {
             System.out.println("First Name = " + user.getFirstName());
             System.out.println("Last Name = " + user.getLastName());
             System.out.println("Email = " + user.getEmail());
-            //System.out.println("Car = " + user.getCar().getModel() + " " + user.getCar().getSeries());
             System.out.printf("Car: model = %s;\n     series = %d \n", user.getCar().getModel(), user.getCar().getSeries());
             System.out.println();
         }
